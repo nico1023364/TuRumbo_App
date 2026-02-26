4c7253622b678393d8c7f8176660a2ded5d37035
nico1023364 <nialfonso12nick@gmail.com>
2026-02-26 14:53:09 -0500

    Primer Commit - Carga de proyecto

---
+++ app/src/main/java/com/example/apptaller2/Activities/InicioActivity.kt
@@ -0,0 +1,23 @@
+package com.example.apptaller2.Activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.apptaller2.R
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inicio)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        },3000)
+    }
+}
