52808de680a551416ba4e3a39d3a35f7989d5f53
nico1023364 <nialfonso12nick@gmail.com>
2026-03-06 21:49:13 -0500

    LinearLayouts en todas las activities y adicion de activity de Registro

--- app/src/main/java/com/example/apptaller2/Activities/RegisterActivity.kt
+++ app/src/main/java/com/example/apptaller2/Activities/RegisterActivity.kt
@@ -1,23 +1,21 @@
 package com.example.apptaller2.Activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.apptaller2.R
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, RegisterActivity::class.java))
-            finish()
-        },3000)
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
